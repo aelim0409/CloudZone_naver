e584391c7ac995bf47710fb4a64c460141449724
aelim0409 <72659915+aelim0409@users.noreply.github.com>
2022-05-28 22:59:33 +0900

    main map 버튼 추가

--- app/src/main/java/com/example/cloudzone_naver/MainActivity.java
+++ app/src/main/java/com/example/cloudzone_naver/MainActivity.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.util.FusedLocationSource;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
     private MapView mapView;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
         naverMapBasicSettings();
+        locationSource =
+                new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
     }
     public void naverMapBasicSettings() {
@@ -27,15 +34,33 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,  @NonNull int[] grantResults) {
+
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
+    @Override
     public void onMapReady(@NonNull final NaverMap naverMap) {
 
+        naverMap.setLightness(0.3f);
 // 현재 위치 버튼 안보이게 설정
         UiSettings uiSettings = naverMap.getUiSettings();
 
         uiSettings.setLocationButtonEnabled(true);
 
-        // 지도 유형 위성사진으로 설정 -> 잘 안나옴
-        naverMap.setMapType(NaverMap.MapType.Satellite);
-
+        // 지도 유형 위성사진으로 설정 ->
+      //  naverMap.setMapType(NaverMap.MapType.Satellite);
+        // 위치 setlocation
+        naverMap.setLocationSource(locationSource);
+        //트래킹 모두 카메라가 따라감
+        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
     }
 }

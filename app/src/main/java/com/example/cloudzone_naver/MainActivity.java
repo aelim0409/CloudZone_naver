3500d6f392db4ccaf5d263806527280b4245cf29
surakim <srsw000521@gmail.com>
2022-06-02 18:23:41 +0900

    Retrofit2 연동 smoking

--- app/src/main/java/com/example/cloudzone_naver/MainActivity.java
+++ app/src/main/java/com/example/cloudzone_naver/MainActivity.java
@@ -3,18 +3,24 @@ package com.example.cloudzone_naver;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
 import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.CircleOverlay;
 import com.naver.maps.map.util.FusedLocationSource;
 
+import java.util.ArrayList;
 import java.util.List;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,8 +38,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private FusedLocationSource locationSource;
     private NaverMap naverMap;
 
-    private final String BASE_URL = "http://13.125.51.242:8000";
+    private final String BASE_URL = "http://13.125.51.242:8000/";
     private MyApi mMyAPI;
+    private MyApi_smoking mMyAPI2;
+
+    public List<PostItem> nonsmoking = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,6 +58,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
 
         initMyAPI(BASE_URL);
+        initMyAPI_smoking(BASE_URL);
 
         btn_non_smoke.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,28 +66,26 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
                 Log.d(TAG,"GET");
                 Call<List<PostItem>> getCall = mMyAPI.get_posts();
+
                 getCall.enqueue(new Callback<List<PostItem>>() {
                     @Override
                     public void onResponse(Call<List<PostItem>> call, Response<List<PostItem>> response) {
-
-                        Log.d(TAG,""+ call);
-                        Log.d(TAG,""+ response.body());
-                        Log.d(TAG, ""+response);
                         if( response.isSuccessful()){
                             List<PostItem> mList = response.body();
-                            String result ="";
                             for( PostItem item : mList){
-                                /*if(item.equals("latitude: ")){
-                                    result += item.getLatitude();
-                                }*/
+                                Log.d(TAG, ""+item.getLatitude()+" "+item.getLongitude()+" "+" "+item.getRadius());
+                                nonsmoking.add(new PostItem(item.getLatitude(),item.getLongitude(),item.getRadius()));
                             }
-                            Log.d(TAG, " "+result);
-                            Log.d(TAG,"success");
+                            Log.d(TAG,"non_smoking : success");
                         }else {
                             Log.d(TAG,"Status Code : " + response.code());
                         }
+
                     }
+
+
                     @Override
+
                     public void onFailure(Call<List<PostItem>> call, Throwable t) {
                         Log.d(TAG,"Fail msg : " + t.getMessage());
                     }
@@ -90,6 +98,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             @Override
             public void onClick(View view) {
 
+
+                Log.d(TAG,"GET");
+                Call<List<PostItem>> getCall = mMyAPI2.get_posts();
+
+                getCall.enqueue(new Callback<List<PostItem>>() {
+                    @Override
+                    public void onResponse(Call<List<PostItem>> call, Response<List<PostItem>> response) {
+                        if( response.isSuccessful()){
+                            List<PostItem> mList = response.body();
+                            for( PostItem item : mList){
+                                Log.d(TAG, ""+item.getLatitude()+" "+item.getLongitude()+" "+" "+item.getRadius());
+                            }
+                            Log.d(TAG,"smoking : success");
+                        }else {
+                            Log.d(TAG,"Status Code : " + response.code());
+                        }
+
+                    }
+
+
+                    @Override
+
+                    public void onFailure(Call<List<PostItem>> call, Throwable t) {
+                        Log.d(TAG,"Fail msg : " + t.getMessage());
+                    }
+                });
             }
 
 
@@ -114,6 +148,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                 .build();
 
         mMyAPI = retrofit.create(MyApi.class);
+
+    }
+    private void initMyAPI_smoking(String baseUrl){
+        Log.d(TAG,"initMyAPI_smoking : " + baseUrl);
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        mMyAPI2 = retrofit.create(MyApi_smoking.class);
     }
 
     public void naverMapBasicSettings() {
@@ -134,6 +177,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onRequestPermissionsResult(
                 requestCode, permissions, grantResults);
     }
+
     @Override
     public void onMapReady(@NonNull final NaverMap naverMap) {
 
@@ -146,9 +190,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         // 지도 유형 위성사진으로 설정 ->
         //  naverMap.setMapType(NaverMap.MapType.Satellite);
         // 위치 setlocation
-        naverMap.setLocationSource(locationSource);
+        // naverMap.setLocationSource(locationSource);
         //트래킹 모두 카메라가 따라감
-        naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-    }
+        // naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
 
+        CameraPosition cp = new CameraPosition(
+                new LatLng(37.4,127.075),
+                16);
+
+        NaverMapOptions options = new NaverMapOptions().camera(cp);
+
+        CircleOverlay circle = new CircleOverlay();
+        circle.setCenter(new LatLng(37.5666102, 126.9783881));
+        circle.setRadius(500);
+        circle.setColor(Color.GREEN);
+        circle.setVisible(true);
+        Log.d(TAG,"naverMap : "+naverMap);
+        circle.setMap(naverMap);
+    }
 }
